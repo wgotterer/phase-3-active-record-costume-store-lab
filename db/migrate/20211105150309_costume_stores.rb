3f45b55b36ac6cd98e13527196895a71354e9a19
William Gotterer <william.gotterer@gmail.com>
2021-11-05 11:12:42 -0400

    Done.

---
+++ db/migrate/20211105150309_costume_stores.rb
@@ -0,0 +1,24 @@
+class CostumeStores < ActiveRecord::Migration[6.1]
+  def change
+
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :is_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time 
+      t.timestamps
+    end
+  end
+end
+
+
+# name (string)
+# location (string)
+# costume_inventory (integer)
+# num_of_employees (integer)
+# is_in_business (boolean)
+# opening_time (datetime)
+# closing_time (datetime)
